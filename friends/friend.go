638075f590390b4b92e7caa8fbf2177c991dc64f
weekover <799465224@qq.com>
2024-05-10 12:09:37 +0800

    go module

--- friends/friend.go
+++ friends/friend.go
@@ -13,16 +13,16 @@ func main() {
     // the time, source file, and line number.
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
+    
+    // A slice of names.
+    names := []string{"Gladys", "Samantha", "Darrin"}
 
-    // Request a greeting message.
-    message, err := greetings.Hello("")
-    // If an error was returned, print it to the console and
-    // exit the program.
+    // Request greeting messages for the names.
+    messages, err := greetings.Hellos(names)
     if err != nil {
         log.Fatal(err)
     }
-
-    // If no error was returned, print the returned message
-    // to the console.
-    fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console.
+    fmt.Println(messages)
 }
